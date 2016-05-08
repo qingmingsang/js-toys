1297bd489bc40ac17696f407b2d1a5f49c7782fd
qingming <358242939@qq.com>
2016-05-08 23:37:58 +0800

    errorDetect

--- QM.js
+++ QM.js
@@ -1,4 +1,8 @@
 var QM = (function() {
+	/*
+	 * QM()
+	 * (string Selector,['click'etc],[function],['on'|'off'])
+	 */
 	var $ = function(Selector, Action, Func, Bind) {
 		if (Action) {
 			var i = 0;
@@ -20,3 +24,37 @@ var QM = (function() {
 	}
 	return $;
 })();
+(function($) {
+	/*
+	 * QM.errorDetect()
+	 */
+
+	function stringDetect(str) {
+		var errorData = ['null', 'false', 'error', '0', 'undefined', 'NaN', '[]', '{}', ''];
+		for (var i = 0; i < errorData.length; i++) {
+			if (str === errorData[i]) {
+				return false;
+			}
+		}
+		return str;
+	}
+
+	function objectDetect(obj) {
+		if (obj === null || (obj instanceof Array && obj.length === 0) || (Object.prototype.isPrototypeOf(obj) && Object.keys(obj).length === 0)) {
+			return false;
+		} else {
+			return obj;
+		}
+	}
+	$.errorDetect = function(data) {
+		switch (typeof data) {
+			case 'string':
+				return stringDetect(data);
+			case 'object':
+				return objectDetect(data);
+			default:
+				return data;
+		}
+	}
+	return $;
+})(QM);
