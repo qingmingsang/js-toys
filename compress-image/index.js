c33a384d4b4f316d514c1a61ea36d9da5f9c83ab
qingming <358242939@qq.com>
2017-08-25 14:57:45 +0800

    key

--- compress-image/index.js
+++ compress-image/index.js
@@ -6,9 +6,7 @@ const pinyin = require('pinyin');
 const dir = './src';
 const output = './dist';
 /*须在tinypng.com上申请一个key,每个key每个月有500的限额,超出部分需要付费*/
-tinify.key = 'Q24nyI4Hl23zA1an2SRkSUJmhyZRGezd';
-//tinify.key = 'kiFnI8fLANQyApLKzgBvlKodP2WdHxcX';
-//tinify.key = 'MMkxUOv7d9wkj_Vp38gI3R8lN8O72T7F';
+tinify.key = 'pZVSu84osiTBFnyvIgC6d8P4yT3lbgMx';
 
 function checkFile(filePath) {
     if (!fs.existsSync(filePath)) {
